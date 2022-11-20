d908ff1ce21c8379605913547a3920c3e06ec80d
KelvinBrenand <brenand.kelvin@gmail.com>
2022-11-20 12:46:54 -0300

    Changed the name so i can publish on pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="image_processing",
+    name="image_processing-KBrenand",
     version="0.0.1",
     author="Kelvin Brenand",
     author_email="brenand.kelvin@gmail.com",
